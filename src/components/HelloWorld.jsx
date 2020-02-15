bdd0469252b5c39de0eb5e7336786cc9d7cd556c
william.ws7} <alexander171296@gmail.com>
2020-02-15 11:08:35 -0400

    first commit

---
+++ src/components/HelloWorld.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const HelloWorld =() =>(
+    <h1>hola</h1>
+);
+
+export default HelloWorld;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Apl from './containers/App'
+
+ReactDOM.render(<Apl/>,document.getElementById('app'));
