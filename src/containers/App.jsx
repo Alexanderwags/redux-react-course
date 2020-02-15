67e12910416d383a3937c41113ab349249995389
william.ws7} <alexander171296@gmail.com>
2020-02-15 12:25:20 -0400

    agg img

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -4,49 +4,44 @@ import Search from '../components/Search';
 import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
-import Footer from '../components/Footer'
-import '../assets/styles/app.scss';
-import '../assets/styles/components/Search.scss';
-import '../assets/styles/components/Header.scss';
+import Footer from '../components/Footer';
+import useInitialState from '../hooks/useInitialState';
+import '../assets/styles/App.scss';
+
+const API = 'http://localhost:3000/initalState'
+
 const App = () => {
-    
-    const [videos ,setVideos]=useState([]);
-    useEffect( () =>{
-
-        fetch('http://localhost:3000/initalState')
-          .then(response => response.json())
-          .then(data => setVideos(data));
-
-    },[]);
-   
-    return (
-
-        <div className="App">
-            <Header />
-            <Search />
-            {
-                videos.mylist.length > 0 &&
-                <Categories>
-                    <Carousel>
-                        <CarouselItem />
-
-                    </Carousel>
-                </Categories>
-
-            }
-            <Categories title="Tendencias">
-                <Carousel>
-                    {
-                        videos.trends.map(item =>
-                            <CarouselItem key="item.id" {...item}/>
-                        )
-                    }
-
-
-                </Carousel>
-            </Categories>
-            <Footer />
-        </div>
-    );
+  const initialState = useInitialState(API);
+  return initialState.length === 0 ? <h1>Loading...</h1> : (
+    <div className="App">
+      <Header />
+      <Search />
+      {initialState.mylist.length > 0 &&
+        <Categories title="Mi Lista">
+          <Carousel>
+            {initialState.mylist.map(item =>
+              <CarouselItem key={item.id} {...item} />
+            )}
+          </Carousel>
+        </Categories>
+      }
+      <Categories title="Tendencias">
+        <Carousel>
+          {initialState.trends.map(item =>
+            <CarouselItem key={item.id} {...item} />
+          )}
+        </Carousel>
+      </Categories>
+      <Categories title="Originales de Platzi Video">
+        <Carousel>
+          {initialState.originals.map(item =>
+            <CarouselItem key={item.id} {...item} />
+          )}
+        </Carousel>
+      </Categories>
+      <Footer />
+    </div>
+  );
 }
+
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Apl from './containers/App'
+import App from './containers/App';
 
-ReactDOM.render(<Apl/>,document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
