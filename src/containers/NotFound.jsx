d283fce04119e5d6d82b341bfea079d6156f176d
griecomv <mariojosuexz@gmail.com>
2020-02-20 16:25:23 -0400

    agg class

---
+++ src/containers/NotFound.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const NotFound = () =>(
+
+    <>
+         
+        <h1>No Encontrado</h1>
+        <h2>Regresa al Home</h2>
+
+    </>
+);
+
+export default NotFound;
